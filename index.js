139b6b5f151364399297de866ab4a21a61dfd006
ssk101 <steelskysoftware@gmail.com>
2022-01-14 12:30:02 +0100

    init2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {
   SocketServer, SocketClient,
 } from '@ssk101/facade-sockets'
-import { routes } from './routes.js'
+// import { routes } from './routes.js'
 
 const server = new SocketServer({ port: 3099 })
+const client = new SocketClient({ port: 3099 })
