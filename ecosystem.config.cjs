dc56aee573dcc18979c555c25f3806ff42274ae7
ssk101 <steelskysoftware@gmail.com>
2022-04-19 17:13:14 +0200

    cfg

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,26 +1,23 @@
 module.exports = {
-  apps: [
-    {
-      name: 'lansocket-server',
-      script: './index.js',
-      watch: true,
-      max_memory_restart: '2G',
-      autorestart: true,
-      restart_delay: 30000,
-      node_args: [
-        '--experimental-json-modules',
-      ],
-    },
-    {
-      name: 'lansocket-jobs',
-      script: './jobs/index.mjs',
-      watch: true,
-      max_memory_restart: '1G',
-      autorestart: true,
-      restart_delay: 30000,
-      node_args: [
-        '--experimental-json-modules',
-      ],
-    },
-  ]
+  apps : [{
+    name: 'lansocket-server',
+    script: './index.js',
+    watch: true,
+    max_memory_restart: '2G',
+    autorestart: true,
+    restart_delay: 30000,
+    node_args: [
+      '--experimental-json-modules',
+    ],
+  }, {
+    name: 'lansocket-jobs',
+    script: './jobs/index.mjs',
+    watch: true,
+    max_memory_restart: '1G',
+    autorestart: true,
+    restart_delay: 30000,
+    node_args: [
+      '--experimental-json-modules',
+    ],
+  }],
 }
